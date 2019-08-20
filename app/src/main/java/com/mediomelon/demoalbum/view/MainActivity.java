98db366feb63f7755601d64088fe7ec84ca3c8cd
Joseph <rjuarez@mediomelon.mx>
2019-08-20 12:41:54 -0500

    Primer commit

---
+++ app/src/main/java/com/mediomelon/demoalbum/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mediomelon.demoalbum.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mediomelon.demoalbum.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
