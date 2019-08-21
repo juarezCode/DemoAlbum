84f641e1cf08e487439b3ab288eebba4f9a34c52
GustavoSH <greyes@mediomelon.mx>
2019-08-21 18:51:33 -0500

    Creación bottomNavigationView
    Se integro bottomNavigationView a MainActivity

--- app/src/main/java/com/mediomelon/demoalbum/view/MainActivity.java
+++ app/src/main/java/com/mediomelon/demoalbum/view/MainActivity.java
@@ -1,15 +1,54 @@
 package com.mediomelon.demoalbum.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mediomelon.demoalbum.R;
+import com.mediomelon.demoalbum.view.fragments.AlbumFragment;
+import com.mediomelon.demoalbum.view.fragments.UserFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new UserFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = menuItem -> {
+        Fragment fragment;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_users:
+                fragment = new UserFragment();
+                loadFragment(fragment);
+                return true;
+
+            case R.id.navigation_albums:
+                fragment = new AlbumFragment();
+                loadFragment(fragment);
+                return true;
+        }
+        return false;
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout_bottom_navigation, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
